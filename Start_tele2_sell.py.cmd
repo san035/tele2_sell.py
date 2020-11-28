49430120350828829cb9f7e35ab4f04ccbce5500
HP <askorn@mail.ru>
2020-11-28 20:22:15 +0300

    + убран *.lnk

---
+++ Start_tele2_sell.py.cmd
@@ -0,0 +1,7 @@
+cd /d "%~dp0"
+
+python.exe tele2_sell.py
+pause
+
+rem ���������� � ������������
+Rem copy Start_tele2_sell.py.cmd.lnk "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
